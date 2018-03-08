5b8930b9c5f5413c7469adf3f8cb4e3f8061949a
andrea-isabel <andrea19.93.15@gmail.com>
2018-03-07 20:32:55 -0500

    iniciando componente

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './css/Header.css'
 
 class Header extends Component {
   render() {
     return (
-      <header className="container">
+      <header className="container-fluid">
         <div className="row">
           <div className="col-12">
-            <h1>Soy un header</h1>
+            <h1 id="logo"><img src="assets/logo-2.png" width="100" /> </h1>
           </div>
         </div>
       </header>
